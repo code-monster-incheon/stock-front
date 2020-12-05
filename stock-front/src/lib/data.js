0657fbcee63cd0ee79c782500daa40a21a287839
injae.kim <injae.kim.dev@gmail.com>
2020-12-05 22:03:11 +0900

    Fix GET /stocks return value format
    Fix GET /stocks return value format

--- stock-front/src/lib/data.js
+++ stock-front/src/lib/data.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const url = 'http://localhost:8080';
+// const url = 'http://localhost:8080';
+const url = 'https://themaju.herokuapp.com';
 
 export const getData = () => {
   return axios.get(`${url}/stocks`);
